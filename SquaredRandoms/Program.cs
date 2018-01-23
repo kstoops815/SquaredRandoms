635d23c0d4cbd4a676b2d10efc6a24dad000aa27
Kelly Stoops <kstoops815@gmail.com>
2018-01-22 18:42:45 -0600

    list of 20 random numbers created

--- SquaredRandoms/Program.cs
+++ SquaredRandoms/Program.cs
@@ -10,6 +10,20 @@ namespace SquaredRandoms
     {
         static void Main(string[] args)
         {
+            var randomNumberGenerator = new Random();
+            // Create a list
+            var randomNumbers = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                var newNumber = randomNumberGenerator.Next(1, 51);
+                randomNumbers.Add(newNumber);
+                Console.WriteLine(newNumber);
+            }
+
+
+            // Populate the list
+            //randomNumbers.Add
         }
     }
 }
