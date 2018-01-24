dc19432af4042362f5599cfe0fd32b0b759c104d
Kelly Stoops <kstoops815@gmail.com>
2018-01-24 15:29:53 -0600

    completed exercise

--- SquaredRandoms/Program.cs
+++ SquaredRandoms/Program.cs
@@ -13,7 +13,10 @@ namespace SquaredRandoms
             var randomNumberGenerator = new Random();
             // Create a list
             var randomNumbers = new List<int>();
+            var squaredNumbers = new List<int>();
+            var evenOnlyNumbers = new List<int>();
 
+            Console.WriteLine("Random Number List");
             for (int i = 0; i < 20; i++)
             {
                 var newNumber = randomNumberGenerator.Next(1, 51);
@@ -21,9 +24,28 @@ namespace SquaredRandoms
                 Console.WriteLine(newNumber);
             }
 
+            Console.WriteLine("Number Squared List");
+            foreach (var number in randomNumbers)
+            {
+                var squaredNumber = number * number;
+                squaredNumbers.Add(squaredNumber);
+                Console.WriteLine(squaredNumber);
+            }
 
-            // Populate the list
-            //randomNumbers.Add
+            Console.WriteLine("Even Only List");
+
+            foreach (var number in squaredNumbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evenOnlyNumbers.Add(number);
+                    Console.WriteLine(number);
+                }
+            }
+           
+            Console.Read();
+                       
+
         }
     }
 }
